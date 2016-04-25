de26a929811da3d5e7e01df09b5a47fca245fdd9
Dennis Linsley <denlinsley@gmail.com>
2016-04-25 07:46:11 -0700

    Tweak comment.

--- app/components/Notes/NotesList.js
+++ app/components/Notes/NotesList.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotesList = ({ notes }) => (
   <ul className="list-group">
-    {notes.map((note) => ( // parens allow implicit return not on same line
+    {notes.map((note) => ( // parens allow JSX return not on same line
       <li key={note['.key']} className="list-group-item">{note['.value']}</li>
     ))}
   </ul>
